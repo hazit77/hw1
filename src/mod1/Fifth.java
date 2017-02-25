9d8d32a658303473cb1a7f3e19f05d7f90ba14a5
hazit77 <andriyhazit@gmail.com>
2017-02-25 16:45:41 +0200

    Initial commit

---
+++ src/mod1/Fifth.java
@@ -0,0 +1,11 @@
+package mod1;
+
+/**
+ * Created by hazit on 25.02.17.
+ */
+public class Fifth {
+    public static void main(String[] args) {
+        System.out.println("Мій перший клас на Java");
+
+    }
+}
---
+++ src/mod1/Fourth.java
@@ -0,0 +1,10 @@
+package mod1;
+
+/**
+ * Created by hazit on 25.02.17.
+ */
+public class Fourth {
+    public static void main(String[] args) {
+        System.out.println("Мій перший клас на Java");
+    }
+}
